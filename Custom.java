f0f9b319194ac4c8e2c80288edf3846854e681df
nishant <nishantshah1729@gmail.com>
2022-07-25 10:31:06 +0530

    m

--- Custom.java
+++ Custom.java
@@ -1,5 +1,21 @@
-public class Custom {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class Custom {
+    static class Edge{
+        int src;
+        int nbr;
+        int wt;
+
+        public Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+        }
+    }
+    public static void main(String[] args) {
+        ArrayList<Edge>[] graph=new ArrayList[7];
+        for (int i = 0; i < 7; i++) {
+            graph[i]=new ArrayList<>();
+        }
     }
 }
